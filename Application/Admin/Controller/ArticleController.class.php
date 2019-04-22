7da1bef2d3d6adc9f6f5a5e96d2482a536ff10cd
leobin001@163.com <leobin001@163.com>
2019-04-22 12:00:13 +0800

    前端首页和列表页

--- Application/Admin/Controller/ArticleController.class.php
+++ Application/Admin/Controller/ArticleController.class.php
@@ -13,22 +13,22 @@ class ArticleController extends Controller {
     }
 
     public function articleData() {
-        $where = ['status' => 0];
+        $where = array('status' => 0);
         $total = $this->_model->where($where)->count();
         $rows = [];
         if ($total) {
             $rows = $this->_model->alias('a')->field('a.article_id,b.category_name,a.category_id,a.title,a.read,a.like,a.update_time')->join('LEFT JOIN category as b on a.category_id = b.category_id')->where(['a.status' => 0])->select();
             foreach ($rows as &$rowVal) {
                 $rowVal['update_time'] = date('Y-m-d H:i:s', $rowVal['update_time']);
-                $rowVal['checkbox'] = "<input type=checkbox name='checkbox' value='" . base64_encode($rowVal['category_id']) . "'>";
-                $rowVal['operate'] = "<div class='col-sm-1'><span data-id=" . base64_encode($rowVal['category_id']) . " class='glyphicon glyphicon-edit' title='编辑当前'></span></div>";
-                $rowVal['operate'] .= "<div class='col-sm-1'><span data-id=" . base64_encode($rowVal['category_id']) . " class='glyphicon glyphicon-trash'  title='删除当前'></span></div>";
+                $rowVal['checkbox'] = "<input type=checkbox name='checkbox' value='" . base64_encode($rowVal['article_id']) . "'>";
+                $rowVal['operate'] = "<div class='col-sm-1'><span data-id=" . base64_encode($rowVal['article_id']) . " class='glyphicon glyphicon-edit' title='编辑当前'></span></div>";
+                $rowVal['operate'] .= "<div class='col-sm-1'><span data-id=" . base64_encode($rowVal['article_id']) . " class='glyphicon glyphicon-trash'  title='删除当前'></span></div>";
             }
         }
-        $data = [
+        $data = array(
             'total' => $total ? $total : 0,
             'rows' => $rows,
-        ];
+        );
 
         $this->ajaxReturn($data);
     }
@@ -36,19 +36,20 @@ class ArticleController extends Controller {
     public function edit() {
         if (IS_POST) {
             $post = I('post.');
-            $data = [
+            $data = array(
                 'category_id'=>$post['category_id'],
                 'title'=>$post['title'],
                 'list_pic'=>$post['list_pic'],
                 'content' => $post['content'],
                 'update_time'=>time()
-            ];
+            );
             if ($post['id']) {
                 //修改
-                $where = [
-                    'article_id' => $post['id'],
+                $where = array(
+                    'article_id' => base64_decode($post['id']),
                     'status' => 0
-                ];
+                );
+
                 $result = $this->_model->where($where)->save($data);
             } else {
                 //新增
@@ -57,22 +58,23 @@ class ArticleController extends Controller {
             }
 
             if ($result) {
-                $this->ajaxReturn(['code' => 1001, 'msg' => '修改成功']);
+                $this->ajaxReturn(array('code' => 1001, 'msg' => '修改成功'));
             } else {
-                $this->ajaxReturn(['code' => -1001, 'msg' => '修改失败']);
+                $this->ajaxReturn(array('code' => -1001, 'msg' => '修改失败'));
             }
         }
 
         if (I('get.id')) {
             $articleId = base64_decode(I('get.id'));
-            $row = $this->_model->field('article_id,category_id,title,list_pic,content')->where(['category_id' => $articleId])->find();
-            $row['list_pic'] = 'http://'. $_SERVER['HTTP_HOST'] . '/' . $row['list_pic'];
+            $row = $this->_model->field('article_id,category_id,title,list_pic,content')->where(array('article_id' => $articleId))->find();
+            $row['show_list_pic'] = 'http://'. $_SERVER['HTTP_HOST'] . '/' . $row['list_pic'];
             $row['content'] = htmlspecialchars_decode($row['content']);
+            $row['article_id'] = base64_encode($row['article_id']);
             //dump($row);
             $this->assign('row', $row);
         }
 
-        $categoryList = M('category')->field('category_id, category_name')->where(['parent_id' => 0,'status' => 0])->select();
+        $categoryList = M('category')->field('category_id, category_name')->where(array('parent_id' => 0,'status' => 0))->select();
         $this->assign('categoryList', $categoryList);
 
         $this->display();
--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -2,18 +2,35 @@
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
+
     public function index(){
+        $this->getCategoryList();
         $this->display();
-
-
         //$this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px } a,a:hover{color:blue;}</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>版本 V{$Think.version}</div><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_55e75dfae343f5a1"></thinkad><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
     }
 
-    public function listing() {
+    public function articleList() {
+        $categoryId = I('get.category_id');
+        $articleList = M('article')->field('article_id, title, list_pic, read, like, update_time')->where(array('status'=>0, 'category_id'=>$categoryId))->select();
+        foreach ($articleList as &$article) {
+            $article['list_pic'] = 'http://'. $_SERVER['HTTP_HOST'] . '/' . $article['list_pic'];
+        }
+        $this->assign('articleList', $articleList);
+
+        $this->getCategoryList();
         $this->display();
     }
 
     public function detail() {
+        $this->getCategoryList();
         $this->display();
+    }
+
+    /**
+     * 获取目录列表
+     */
+    private function getCategoryList() {
+        $categoryList = M('category')->field('category_id, category_name')->where(array('status'=>0, 'parent_id'=>0))->select();
+        $this->assign('categoryList', $categoryList);
     }
 }
